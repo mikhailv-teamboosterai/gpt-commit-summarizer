5895a651d45d0ded763edc597793f1b7e250ed7c
mikhailv-teamboosterai <139373270+mikhailv-teamboosterai@users.noreply.github.com>
2023-10-11 22:05:43 -0400

    Update commitSummary.ts

--- src/commitSummary.ts
+++ src/commitSummary.ts
@@ -239,13 +239,13 @@ export async function summarizeCommits(
       console.error(error);
     }
     const comment = `GPT summary of ${headCommit}:\n\n${headCommitShaAndSummary[1]}\n\nPR summary so far:\n\n${prSummary}`;
-    await octokit.issues.createComment({
-      owner: repository.owner.login,
-      repo: repository.name,
-      issue_number: pullNumber,
-      body: comment,
-      commit_id: headCommit,
-    });
+    // await octokit.issues.createComment({
+    //   owner: repository.owner.login,
+    //   repo: repository.name,
+    //   issue_number: pullNumber,
+    //   body: comment,
+    //   commit_id: headCommit,
+    // });
   }
   return commitSummaries;
 }
