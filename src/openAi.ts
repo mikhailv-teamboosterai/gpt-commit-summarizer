c93aa9b51e59694b214faec129525030b99e508d
Mike <mikhail@teamboosterai.com>
2023-10-11 23:04:40 -0400

    upd

--- src/openAi.ts
+++ src/openAi.ts
@@ -5,7 +5,7 @@ const configuration = new Configuration({
 });
 
 export const MAX_OPEN_AI_QUERY_LENGTH = 20000;
-export const MODEL_NAME = "text-davinci-003";
+export const MODEL_NAME = "gpt-3.5-turbo";
 export const TEMPERATURE = 0.5;
 export const MAX_TOKENS = 512;
 
