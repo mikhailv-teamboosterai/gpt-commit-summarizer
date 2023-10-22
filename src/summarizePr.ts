86c22a17879c9e3df2a7ddf520f10967867f1e5e
Mike <mikhail@teamboosterai.com>
2023-10-21 22:38:57 -0400

    upd

--- src/summarizePr.ts
+++ src/summarizePr.ts
@@ -1,3 +1,4 @@
+import { ChatCompletionRequestMessage } from "openai";
 import {
   MAX_OPEN_AI_QUERY_LENGTH,
   MAX_TOKENS,
@@ -6,13 +7,17 @@ import {
   TEMPERATURE,
 } from "./openAi";
 
-const OPEN_AI_PROMPT = `You are an expert programmer, and you are trying to summarize a pull request.
+const OPEN_AI_PROMPT: ChatCompletionRequestMessage = {
+  role: "system",
+  content: 
+`You are an expert programmer, and you are trying to summarize a pull request.
 You went over every commit that is part of the pull request and over every file that was changed in it.
 For some of these, there was an error in the commit summary, or in the files diff summary.
 Please summarize the pull request. Write your response in bullet points, starting each bullet point with a \`*\`.
 Write a high level description. Do not repeat the commit summaries or the file summaries.
 Write the most important bullet points. The list should not be more than a few bullet points.
-`;
+`
+};
 
 const linkRegex = /\[.*?\]\(https:\/\/github\.com\/.*?[a-zA-Z0-f]{40}\/(.*?)\)/;
 
@@ -38,9 +43,13 @@ export async function summarizePr(
   const filesString = Object.entries(fileSummaries)
     .map(([filename, summary]) => `File ${filename}:\n${summary}`)
     .join("\n");
-  const openAIPrompt = `${OPEN_AI_PROMPT}\n\nTHE COMMIT SUMMARIES:\n\`\`\`\n${commitsString}\n\`\`\`\n\nTHE FILE SUMMARIES:\n\`\`\`\n${filesString}\n\`\`\`\n\n
+  const openAIPrompt: ChatCompletionRequestMessage[] = [
+    OPEN_AI_PROMPT,
+    {
+      role: "user",
+      content: `THE COMMIT SUMMARIES:\n\`\`\`\n${commitsString}\n\`\`\`\n\nTHE FILE SUMMARIES:\n\`\`\`\n${filesString}\n\`\`\`\n\n
   Reminder - write only the most important points. No more than a few bullet points.
-  THE PULL REQUEST SUMMARY:\n`;
+  THE PULL REQUEST SUMMARY:`}];
   console.log(`OpenAI for PR summary prompt:\n${openAIPrompt}`);
 
   if (openAIPrompt.length > MAX_OPEN_AI_QUERY_LENGTH) {
@@ -48,15 +57,18 @@ export async function summarizePr(
   }
 
   try {
-    const response = await openai.createCompletion({
+    const response = await openai.createChatCompletion({
       model: MODEL_NAME,
-      prompt: openAIPrompt,
+      messages: openAIPrompt,
       max_tokens: MAX_TOKENS,
       temperature: TEMPERATURE,
     });
 
-    console.log(`\n${response.data.choices[0].text}`);
-    return response.data.choices[0].text ?? "Error: couldn't generate summary";
+    console.log(`\n${response.data.choices[0].message?.content}`);
+    return (
+      response.data.choices[0].message?.content ??
+      "Error: couldn't generate summary"
+    );
   } catch (error) {
     console.error(JSON.stringify(error));
     return "Error: couldn't generate summary";
